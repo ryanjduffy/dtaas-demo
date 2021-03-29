025fa01b1d2a232ada95a3b061a262b165b2ed9f
JayCanuck <robitaille88@gmail.com>
2021-03-29 08:01:33 -0700

    Demo updates which occurred while Ryan was away

--- components/DataGrid.js
+++ components/DataGrid.js
@@ -2,28 +2,20 @@ import { Autocomplete } from "@material-ui/lab";
 import { TextField } from "@material-ui/core";
 import React from "react";
 
+import typeToLabel from "../util/eventTypes";
+
 import css from "./DataGrid.module.css";
 
-const mapTypeToLabel = (type) => {
-  const map = {
-    pothole: "Potholes",
-    construction: "Road Construction",
-    emergency: "Emergency Response",
-    bicycle: "Bicycles",
-    accident: "Car Accident"
-  };
-
-  return map[type];
-};
-
 const Grid = ({ data, onSelect, selected }) => {
-  const rows = Object.keys(data);
+  const rows = Object.keys(data).sort((a, b) =>
+    typeToLabel(a).localeCompare(typeToLabel(b))
+  );
   return (
     <Autocomplete
       className={css.datagrid}
       id="combo-box-demo"
       options={rows}
-      getOptionLabel={(option) => mapTypeToLabel(option)}
+      getOptionLabel={(option) => typeToLabel(option)}
       style={{ width: 300 }}
       selected={selected}
       onChange={(ev, value) => {
